57ed986e4fd5efe9159bbe50dc2c8c333d201db3
Eliecer Molina <eliecer.molina@uruit.com>
2023-08-01 11:24:40 -0500

    CHECKPOINT: adding middleware example

--- controllers/health/index.ts
+++ controllers/health/index.ts
@@ -5,6 +5,7 @@ import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import { HandlerType } from 'simple-lambda-api-router/dist/classes/AwsSamRouter';
 import { createController } from 'simple-lambda-api-router/dist/utils/controller';
+import auditLogMiddleware from '../../middlewares/AuditLog.middleware';
 
 async function get(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return { statusCode: 200, body: JSON.stringify({ event }) };
@@ -14,7 +15,7 @@ async function post(event: APIGatewayProxyEvent, context: Context): Promise<APIG
   return { statusCode: 200, body: JSON.stringify({ event }) };
 }
 
-const middlewares = [httpErrorHandler(), httpEventNormalizer(), httpJsonBodyParser()];
+const middlewares = [httpErrorHandler(), httpEventNormalizer(), httpJsonBodyParser(), auditLogMiddleware()];
 const controller = createController<HandlerType>({
   get: middy(get).use(middlewares) as HandlerType,
   post: middy(post).use(middlewares) as HandlerType,
---
+++ middlewares/AuditLog.middleware.ts
@@ -0,0 +1,35 @@
+class AuditTrail {
+  name!: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  sayHello() {
+    console.log('Hi! Im ' + this.name);
+  }
+}
+
+const before = async (request: any) => {
+  const { event } = request;
+  const { pathParameters } = event;
+  const { type } = pathParameters;
+  request.AuditTrail = new AuditTrail(type ?? 'noname');
+};
+
+const after = async (request: any) => {
+  request.AuditTrail?.sayHello();
+};
+
+const onError = async (request: any) => {
+  if (request.response === undefined) return;
+  return after(request);
+};
+
+const fns = {
+  before: before,
+  after: after,
+  onError: onError,
+};
+
+export default function auditLogMiddleware() {
+  return fns;
+}
